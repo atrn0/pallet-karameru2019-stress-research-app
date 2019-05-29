82ab8dd2b317013d4e71fbf847f7fd0b52c1b91d
Arata Sato <14242642+aratasato@users.noreply.github.com>
2019-05-29 12:28:44 +0900

    クイズの画面作った！！

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,27 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const express = require('express');
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+
+admin.initializeApp(functions.config().firebase);
+
+// get questions
+let questions;
+try {
+  const qFile = fs.readFileSync('./resource/questions.yml', 'utf-8');
+  questions = yaml.safeLoad(qFile);
+  console.log('load questions.yaml!')
+} catch (err) {
+  console.log(err);
+}
+
+
 
 const app = express();
-app.get('/new', (request, response) => {
-  response.send(`${Date.now()}`);
+app.get('/questions.json', (request, response) => {
+  response.json(questions);
 });
 
 
