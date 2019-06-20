4fa4324f903df8a584e66d61db9b570f3f8c7fde
Arata Sato <14242642+aratasato@users.noreply.github.com>
2019-06-20 15:14:58 +0900

    clean git cache

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  div
+    Header
+    #app.container
+      Questions
+</template>
+
+<script>
+import Questions from "./../components/Questions";
+import Header from "./../components/Header";
+export default {
+  components: {
+    Questions,
+    Header
+  }
+};
+</script>
+
+<style lang="scss">
+@import "~/assets/colors.scss";
+.container {
+  width: 95vw;
+  max-width: 600px;
+  align-items: center;
+  white-space: pre-line;
+}
+</style>
